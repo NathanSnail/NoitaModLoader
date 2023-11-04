eec78fc89a97e78d3611d88a3116be3f70b22faa
Forgenet <justpavel2005@mail.ru>
2023-11-04 21:13:29 +0300

    Add context action

--- extension.js
+++ extension.js
@@ -1,25 +1,49 @@
 const vscode = require('vscode');
 const fs = require('fs-extra');
+const path = require('path');
 
 
-const disposable = vscode.commands.registerCommand('extension.buildProject', () => {
-    const projectPath = vscode.workspace.workspaceFolders;
-    
-    const noitaModsPath = vscode.workspace.getConfiguration().get('noitamodloader.modsPath');
-
-    const excludedItems = vscode.workspace.getConfiguration().get('noitamodloader.excludedItems');
-
-    try {
-        fs.copy(projectPath, noitaModsPath, {
-            filter: (src, _) => {
-                const relativePath = vscode.workspace.asRelativePath(src);
-                return !excludedItems.includes(relativePath);
-            }
-        });
-        vscode.window.showInformationMessage('Проект успешно скопирован в noita/mods.');
-    } catch (error) {
-        vscode.window.showErrorMessage('Произошла ошибка при копировании проекта: ' + error.message);
+async function copyFilesToNoitaMods() {
+    // Get the active file's directory
+    const activeEditor = vscode.window.activeTextEditor;
+    if (!activeEditor) {
+      vscode.window.showErrorMessage('No active editor found.');
+      return;
     }
-});
 
-context.subscriptions.push(disposable);
+    const noitaModsPath = vscode.workspace.getConfiguration().get('noitamodloader.modsPath');
+    const excludedItems = vscode.workspace.getConfiguration().get('noitamodloader.excludedItems');
+    const activeFile = activeEditor.document.fileName;
+    const projectDir = path.dirname(activeFile);
+  
+    try {
+      // Copy files from projectDir to NOITA_MODS_PATH
+      const files = fs.readdirSync(projectDir);
+      files.forEach((file) => {
+        if (!excludedItems.includes(file)) {
+          const sourcePath = path.join(projectDir, file);
+          const destinationPath = path.join(noitaModsPath, file);
+          fs.copyFileSync(sourcePath, destinationPath);
+        }
+      });
+  
+      vscode.window.showInformationMessage('Mod files copied successfully.');
+    } catch (error) {
+      vscode.window.showErrorMessage('Error copying mod files: ' + error.message);
+    }
+  }
+
+
+function activate(context) {
+    const disposable = vscode.commands.registerCommand('extension.loadMod', copyFilesToNoitaMods);
+    context.subscriptions.push(disposable);
+}
+
+exports.activate = activate;
+
+function deactivate() {}
+
+module.exports = {
+    activate,
+    deactivate
+  };    
